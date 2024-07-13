2dd3649553f595166025613731832bdd17b475bf
Smita Satapathy <smitasatapathy@Smitas-MacBook-Air.local>
2024-07-13 15:37:02 +0530

    new files

---
+++ Lecture34- Insertion Questions/reverseList.java
@@ -0,0 +1,17 @@
+class reverseList
+{
+    Node reverseLi(Node head)
+    {
+        Node prev = null;
+        Node next = null;
+        Node curr = head;
+        while(curr!=null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
